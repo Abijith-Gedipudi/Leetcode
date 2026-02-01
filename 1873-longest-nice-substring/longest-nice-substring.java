ff62128258abf14302b2eb426965655deeafd21c
Abijith-Gedipudi <125293966+Abijith-Gedipudi@users.noreply.github.com>
2026-02-01 22:37:03 +0530

    Time: 2 ms (86.55%) | Memory: 44.3 MB (35.15%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+     if(s.length()<2)
+     {
+        return "";
+     }
+     HashSet<Character> h=new HashSet<>();
+     for (char c : s.toCharArray()) {
+            h.add(c);
+        }
+     for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i); 
+            if (h.contains(Character.toLowerCase(c)) &&
+                h.contains(Character.toUpperCase(c))) {
+                continue;
+            }
+            String left = longestNiceSubstring(s.substring(0, i));
+            String right = longestNiceSubstring(s.substring(i + 1));
+            return left.length() >= right.length() ? left : right;
+        }
+
+        return s;  
+    }
+}
